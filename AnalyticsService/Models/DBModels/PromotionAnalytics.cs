60845fc61a10ccc7084c342d5c9dbb59ad3b1114
Nirmal <Nirmal@DESKTOP-J6JLKL0>
2020-12-03 20:11:15 +0530

    Implement database and connect with EF

---
+++ AnalyticsService/Models/DBModels/PromotionAnalytics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnalyticsService.Models.DBModels
+{
+    public partial class PromotionAnalytics
+    {
+        public int AnalyticId { get; set; }
+        public int? PromotionId { get; set; }
+        public int? AdvertismentId { get; set; }
+        public int? Count { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string Type { get; set; }
+    }
+}
