719fa7e6b2ce6f00f47b71541e10027b63e35674
Wael Moughrbel <wmoughrbel@routesme.com>
2021-07-28 15:38:46 +0300

    Change pagination default limit back to 10 according to further changes in requirments

--- AdvertisementAnalysisService/Models/Pagination.cs
+++ AdvertisementAnalysisService/Models/Pagination.cs
@@ -3,7 +3,7 @@
     public class Pagination
     {
         public int offset { get; set; } = 1;
-        public int limit { get; set; } = 100;
+        public int limit { get; set; } = 10;
         public int total { get; set; }
     }
 }
